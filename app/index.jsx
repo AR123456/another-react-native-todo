ea72abeb1ef34671c116869eff061dbbc3a2d377
AR123456 <coloradoz99@gmail.com>
2026-08-06 07:51:48 -0400

    add todo

--- app/index.jsx
+++ app/index.jsx
@@ -8,6 +8,15 @@ export default function Index() {
   const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id));
   // text input
   const [text, setText] = useState("");
+  const addTodo = () => {
+    //remove extra spaces
+    if (text.trim()) {
+      // create id for new   - take first todo it array that has the highest number and add 1 to it, it no todos then the number is 1
+      const newId = todos.length > 0 ? todos[0].id + 1 : 1;
+      // create this todo object then spread in the rest
+      setTodos([{ id: newId, title: text, completed: false }, ...todos]);
+    }
+  };
   return (
     <SafeAreaView>
       <View
