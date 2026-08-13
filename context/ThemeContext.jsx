8e43681b0f7245ff185fc050f41dea7bc17b723c
AR123456 <coloradoz99@gmail.com>
2026-08-13 08:22:18 -0400

    theme provider wrap

---
+++ context/ThemeContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+import { Appearance } from "react-native";
+import { Colors } from "@/constants/Colors";
+
+export const ThemeContext = createContext({});
+// child components that will go inside provider
+export const ThemeProvider = ({ children }) => {
+  const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme);
+
+  const theme = colorScheme === "dark" ? Colors.dark : Colors.light;
+
+  return (
+    <ThemeContext.Provider value={{ colorScheme, setColorScheme, theme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
