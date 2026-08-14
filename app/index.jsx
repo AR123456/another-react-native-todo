f6b223edf4520f08f32ab2377f5ef583bc4bf7ec
AR123456 <coloradoz99@gmail.com>
2026-08-14 09:08:37 -0400

    style sheet function

--- app/index.jsx
+++ app/index.jsx
@@ -129,65 +129,66 @@ export default function Index() {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-  },
-  inputContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 10,
-    padding: 10,
-    width: "100%",
-    maxWidth: 1024,
-    marginHorizontal: "auto",
-    pointerEvents: "auto",
-  },
-  input: {
-    flex: 1,
-    borderColor: "gray",
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginRight: 10,
-    fontSize: 18,
-    fontFamily: "Inter_500Medium",
-    minWidth: 0,
-    color: "white",
-  },
-  addButton: {
-    backgroundColor: "white",
-    borderRadius: 5,
-    padding: 10,
-  },
-  addButtonText: {
-    fontSize: 18,
-    color: "black",
-  },
-  // for flat list of items
-  todoItem: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    gap: 4,
-    padding: 10,
-    borderBottomColor: "gray",
-    borderBottomWidth: 1,
-    width: "100%",
-    maxWidth: 1024,
-    marginHorizontal: "auto",
-    pointerEvents: "auto",
-  },
-  todoText: {
-    flex: 1,
-    fontSize: 18,
-    color: "white",
-    fontFamily: "Inter_500Medium",
-  },
-  completedText: {
-    textDecorationLine: "line-through",
-    color: "gray",
-  },
-});
+function createStyles(theme, colorScheme) {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "black",
+    },
+    inputContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      marginBottom: 10,
+      padding: 10,
+      width: "100%",
+      maxWidth: 1024,
+      marginHorizontal: "auto",
+      pointerEvents: "auto",
+    },
+    input: {
+      flex: 1,
+      borderColor: "gray",
+      borderWidth: 1,
+      borderRadius: 5,
+      padding: 10,
+      marginRight: 10,
+      fontSize: 18,
+      fontFamily: "Inter_500Medium",
+      minWidth: 0,
+      color: "white",
+    },
+    addButton: {
+      backgroundColor: "white",
+      borderRadius: 5,
+      padding: 10,
+    },
+    addButtonText: {
+      fontSize: 18,
+      color: "black",
+    },
+    // for flat list of items
+    todoItem: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      gap: 4,
+      padding: 10,
+      borderBottomColor: "gray",
+      borderBottomWidth: 1,
+      width: "100%",
+      maxWidth: 1024,
+      marginHorizontal: "auto",
+      pointerEvents: "auto",
+    },
+    todoText: {
+      flex: 1,
+      fontSize: 18,
+      color: "white",
+      fontFamily: "Inter_500Medium",
+    },
+    completedText: {
+      textDecorationLine: "line-through",
+      color: "gray",
+    },
+  });
+}
