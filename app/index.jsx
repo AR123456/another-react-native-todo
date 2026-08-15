4d7dc55288ff6336fe8acfca9fee4aa7cb7af4b4
AR123456 <coloradoz99@gmail.com>
2026-08-15 07:43:48 -0400

    seeing icon but on every line

--- app/index.jsx
+++ app/index.jsx
@@ -78,7 +78,7 @@ export default function Index() {
         }
         style={{ marginLeft: 10 }}
       >
-        {/* {colorScheme === "dark" ? (
+        {colorScheme === "dark" ? (
           <Octicons
             name="moon"
             size={36}
@@ -94,14 +94,14 @@ export default function Index() {
             selectable={undefined}
             style={{ width: 36 }}
           ></Octicons>
-        )} */}
-        <Octicons
+        )}
+        {/* <Octicons
           name={colorScheme === "dark" ? "moon" : "sun"}
           size={36}
           color={theme.text}
           selectable={undefined}
           style={{ width: 36 }}
-        ></Octicons>
+        ></Octicons> */}
       </Pressable>
     </View>
   );
