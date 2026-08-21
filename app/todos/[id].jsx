357612ae793136bef75ff73afa8e77844e0330d4
AR123456 <coloradoz99@gmail.com>
2026-08-21 09:48:07 -0400

    todos route folder in app

--- app/todos/[id].jsx
+++ app/todos/[id].jsx
@@ -1,13 +1,1 @@
 // for dynamic route
-import { useLocalSearchParams } from "expo-router";
-import { View, Text, StyleSheet, Pressable, TextInput } from "react-native";
-import { useState, useContext, useEffect } from "react";
-export default function EditScreen() {
-  // when hook called id value passed to route
-  const { id } = useLocalSearchParams();
-  return (
-    <View>
-      <Text>{id}</Text>
-    </View>
-  );
-}
