2ffeab26c03d019c7ce49a6c1933af67ba62e978
AR123456 <coloradoz99@gmail.com>
2026-08-22 06:33:49 -0400

    return just id value in text for now

--- app/todos/[id].jsx
+++ app/todos/[id].jsx
@@ -1,4 +1,13 @@
 // for dynamic route
 import { useLocalSearchParams } from "expo-router";
+import { View, Text, StyleSheet, Pressable, TextInput } from "react-native";
 
-export default function EditScreen() {}
+export default function EditScreen() {
+  // when hook called id value passed to route
+  const { id } = useLocalSearchParams();
+  return (
+    <View>
+      <Text>{id}</Text>
+    </View>
+  );
+}
