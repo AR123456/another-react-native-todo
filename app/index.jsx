caa11c3ea8d8336c879f89dbb880443c764af8ea
AR123456 <coloradoz99@gmail.com>
2026-08-23 07:42:02 -0400

    trying to navigate

--- app/index.jsx
+++ app/index.jsx
@@ -95,7 +95,7 @@ export default function Index() {
   };
   const handlePress = (id) => {
     // use route to go to a page that shows todo detail
-    alert("regular press");
+    useRouter.navigate("/app/todos/[id].jsx");
   };
   // for each todo item
   const renderItem = ({ item }) => (
