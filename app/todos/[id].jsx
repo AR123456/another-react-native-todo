0c303e2e7cc39c5aa1cf04eacc958c19b56f1d29
AR123456 <coloradoz99@gmail.com>
2026-08-24 06:53:11 -0400

    more needed inports

--- app/todos/[id].jsx
+++ app/todos/[id].jsx
@@ -2,6 +2,11 @@
 import { useLocalSearchParams } from "expo-router";
 import { View, Text, StyleSheet, Pressable, TextInput } from "react-native";
 import { useState, useContext, useEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemeContext } from "@/context/ThemeContext";
+import { StatusBar } from "expo-status-bar";
+import { Inter_500Medium, useFonts } from "@expo-google-fonts/inter";
+
 export default function EditScreen() {
   // when hook called id value passed to route
   const { id } = useLocalSearchParams();
