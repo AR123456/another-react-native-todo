638942ad69edde3bd0411f3cef5dcd1fbacdee2c
AR123456 <coloradoz99@gmail.com>
2026-08-27 06:27:06 -0400

    filter out edited put back

--- app/todos/[id].jsx
+++ app/todos/[id].jsx
@@ -45,10 +45,17 @@ export default function EditScreen() {
   const handleSave = async () => {
     try {
       // push the title change to the list in async storage
-      const saveTodo = { ...todo, title: todo.title };
+      const savedTodo = { ...todo, title: todo.title };
 
       const jsonValue = await AsyncStorage.getItem("TodoApp");
       const storageTodos = jsonValue != null ? JSON.parse(jsonValue) : null;
+      // filter out the id just edited , from that filtered list add the edited version back
+      if (storageTodos && storageTodos.length) {
+        const otherTodos = storageTodos.filter(
+          (todo) => todo.id !== savedTodo.id,
+        );
+        const allTodos = [...otherTodos, savedTodo];
+      }
     } catch (e) {
       console.error(e);
     }
